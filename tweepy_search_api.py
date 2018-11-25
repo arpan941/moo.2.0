c94f18663a4fcfa59b228c318f0e5efcf05b6857
Arpan Gupta <arpan.gupta@kronos.com>
2018-11-25 18:49:39 +0530

    Add file for tweepy Search APi: found a way for adding 'since' parameter

---
+++ tweepy_search_api.py
@@ -0,0 +1,23 @@
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy import API
+from tweepy import Cursor
+import csv
+
+class Authentication:
+    def authenticate(self, tweetsFile, searchQuery):
+        auth = OAuthHandler("YO3onH8vbakEW2ZCzaXbx01VW", "FsupJ0Cu595QPXPswPWbmetm6ktZSy86QXN4TN3aHa3AdrVSU9")
+        auth.set_access_token("42595577-IRgFyBq8btWa50H11GZIwE6EexFjQI2lmLGh5gOTV", "lxhe0unY42ZMF5bH9CLv7K9ioeoiSkM7PaPqQ2tDf6m0v")
+        api = API(auth,wait_on_rate_limit=True)
+        csvFile = open(tweetsFile, 'a')
+        csvWriter = csv.writer(csvFile)
+        for tweet in Cursor(api.search, q=searchQuery, lang="en", since="2018-04-03", count=100).items(): #if you put count inside the cursor method as count=100, you can restrict the no of tweets
+            print(tweet.created_at, tweet.text)
+            csvWriter.writerow([tweet.created_at,tweet.text.encode('utf-8')])
+        tweets=api.search(q="$AAPL")
+
+if __name__ == '__main__':
+    searchQuery = "$AAPL"
+    tweetsFile = "tweets1.csv"
+    auth = Authentication()
+    auth.authenticate(tweetsFile, searchQuery)
